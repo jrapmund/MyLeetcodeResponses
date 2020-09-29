a12e070539c05983e07e54bcdf050df2df9bb3bf
Jacob Rapmund <jwrapmund@gmail.com>
2020-09-29 08:38:37 -0700

    Answer to Count and Say question.

--- Leetcode/Leetcode.h
+++ Leetcode/Leetcode.h
@@ -1,8 +1,46 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 
 namespace Leetcode
 {
+	//#38 Count and Say
+    std::string countAndSay(int& n) {
+        //default state
+        if (n == 1) return "1";
 
+        //recursive call
+        n--;
+        std::string state = countAndSay(n);
+        std::string newState = "";
+
+        //iterators
+        std::string::iterator post, prev, curr;
+        post = prev = curr = state.begin();
+
+        //starting position
+        curr++;
+
+        //creation of new string
+        while (curr != state.end())
+        {
+            //comparing values
+            if (*prev != *curr)
+            {
+                newState += std::to_string(curr - post) + *prev;
+                post = prev = curr;
+            }
+            else
+                prev = curr;
+
+            //increment
+            curr++;
+        }
+
+        //final catch
+        newState += std::to_string(curr - post) + *prev;
+
+        return newState;
+    }
 }
--- Leetcode/Main.cpp
+++ Leetcode/Main.cpp
@@ -2,6 +2,8 @@
 
 int main()
 {
+	int n = 30;
+	std::cout << Leetcode::countAndSay(n);
 
 	return 0;
 }
